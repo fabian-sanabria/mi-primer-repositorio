bb843ac353bf720af997b8560687e95a4c78332c
Fabian sanabria lopez <fabian_1903@hotmail.com>
2026-06-21 11:40:40 -0500

    analizando errores con sonarcloud

--- home/home.js
+++ home/home.js
@@ -1,7 +1,79 @@
-const username = 30;
-const funccion = () => {
-  console.log("Hola mundo");
-};
-if (username === 30) {
+// Variable global innecesaria
+var total = 0;
+
+// Función demasiado larga y con múltiples problemas
+function processUsers(users) {
+  // Variable declarada y nunca utilizada
+  let unusedVariable = "test";
+
+  if (users == null) {
+    console.log("No users");
+  }
+
+  for (let i = 0; i < users.length; i++) {
+    // Comparación débil
+    if (users[i].age == 18) {
+      console.log("User is 18");
+    }
+
+    // Código duplicado
+    if (users[i].country == "CO") {
+      console.log("Colombia");
+    }
+
+    if (users[i].country == "CO") {
+      console.log("Colombia");
+    }
+
+    // Condición siempre verdadera
+    if (true) {
+      total = total + 1;
+    }
+
+    // Uso innecesario de else
+    if (users[i].active) {
+      console.log("Active");
+    } else {
+      console.log("Inactive");
+    }
+  }
+}
+
+// Función con parámetros no utilizados
+function calculate(a, b, c) {
+  return a + b;
+}
+
+// Posible bug
+function getUserName(user) {
+  return user.name.toUpperCase();
+}
+
+// Comparación problemática
+function validate(value) {
+  if (value == undefined) {
+    return false;
+  }
+
+  return true;
+}
+
+// Variable declarada con var
+var name = "Juan";
+
+if (name == "Juan") {
   console.log("Hola Juan");
 }
+
+// Función nunca utilizada
+function oldFunction() {
+  console.log("Deprecated");
+}
+
+processUsers([
+  {
+    age: 18,
+    country: "CO",
+    active: true,
+  },
+]);
